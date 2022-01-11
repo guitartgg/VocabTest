4076b3497aafbd269fecb7e553988f6abbe7218c
GG Guitart <54510422+gguitart@users.noreply.github.com>
2022-01-11 15:25:50 -0600

    created two new components: first to hold a singlular image, the next to hold 4 images

---
+++ new-project/src/components/ImageContainer.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="image-container">IMAGE container</div>
+</template>
+<style>
+.image-container {
+    background: lightgray;
+    width: 300px;
+  height: 250px;
+  border-radius: 25px;
+  margin: 2rem;
+}
+</style>
---
+++ new-project/src/components/ImagesContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Images Container</h1>
+<div class="container">
+    <image-container class="item"/>
+    <image-container class="item"/>
+    <image-container class="item"/>
+    <image-container class="item"/>
+    </div>
+  </div>
+</template>
+<script>
+import ImageContainer from "./ImageContainer";
+
+export default {
+  components: {
+    ImageContainer
+  }
+};
+</script>
+<style>
+.container{
+    display:flex; 
+    flex-direction: row;
+}
+</style>
