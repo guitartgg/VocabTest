3e91126d06b6029ea5e0ed633bfcaf9da3eeb218
GG Guitart <54510422+gguitart@users.noreply.github.com>
2022-01-19 23:16:16 -0800

    initialized firebase

---
+++ new-project/src/index.js
@@ -0,0 +1,7 @@
+import {initializeApp} from 'firebase/app'
+import {} from 'firebase/SERVICENAME'
+import {} from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+   // need connection to firebase here 
+})
